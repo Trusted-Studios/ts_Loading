ba82dff44d10b9f7212cd29207ade8e8e32bd0a7
GMW Scripts | Junior Developer <79488475+GermanWarthog@users.noreply.github.com>
2024-06-01 19:15:50 +0200

    updated fxmanifest version

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -8,7 +8,7 @@ games { 'gta5' }
 author 'Trusted-Studios | GTA:O Loading Script'
 description 'GTA:O Loading Script made by Trusted-Studios'
 repository 'https://shop.trusted-studios.eu'
-version '1.0.0'
+version '1.0.1'
 
 loadingscreen_manual_shutdown 'yes'
 
